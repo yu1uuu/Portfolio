8e456b14330017647e8f090d1aa86bc7aeeebbcb
Yulu Jiang <89326638+yu1uuu@users.noreply.github.com>
2024-06-03 12:59:55 -0400

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,38 @@
 import React from 'react';
-// import './Navbar.css';  // Make sure to create and import a CSS file for styles
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <h1>My Personal Website</h1>
-      <div className="menu">
-        <a href="#projects">Projects</a>
-        <a href="#experience">Experience</a>
-        <a href="#about-me">About Me</a>
-      </div>
+    <nav style={{
+      display: 'flex', 
+      justifyContent: 'space-evenly', 
+      alignItems: 'right', 
+      backgroundColor: '#f8f8f8', 
+      padding: '10px 20px', // Adds padding around the navigation bar
+      boxShadow: '0 2px 4px rgba(0,0,0,0.1)' // Adds a subtle shadow for a touch of depth
+    }}>
+      <a href="#Home" style={{
+        textDecoration: 'none', 
+        color: '#333', 
+        marginRight: '5px', 
+        fontWeight: 'bold'
+      }}>Home</a>
+      <a href="#About" style={{
+        textDecoration: 'none', 
+        color: '#333', 
+        marginRight: '5px', 
+        fontWeight: 'bold'
+      }}>About</a>
+      <a href="#" style={{
+        textDecoration: 'none', 
+        color: '#333', 
+        marginRight: '5px', 
+        fontWeight: 'bold'
+      }}>Paintings</a>
+      <a href="#contact" style={{
+        textDecoration: 'none', 
+        color: '#333', 
+        fontWeight: 'bold'
+      }}>Contact</a>
     </nav>
   );
 }
