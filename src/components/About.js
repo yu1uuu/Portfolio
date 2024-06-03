c7d052e6b87cf3a25a03a6d2c05d75872455a998
yu1uuu <yulujiang00@gmail.com>
2024-06-03 14:15:39 -0400

    added links

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+
+    <h2> uoft </h2>
+    <p>
+        Our mission is what drives us to do everything possible to expand
+        human potential. We do that by creating groundbreaking sport
+        innovations, by making our products more sustainably, by building a
+        creative and diverse global team and by making a positive impact in
+        communities where we live and work. Based in Beaverton, Oregon, NIKE,
+        Inc. includes the Nike, Converse, and Jordan brands.
+    </p>
+ 
+    </div>
+  );
+}
+
+export default About;
