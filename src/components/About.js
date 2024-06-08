0875139e784030d9fafe56d3aa710d369d8bafa7
yu1uuu <yulujiang00@gmail.com>
2024-06-08 12:33:32 -0400

    changed bqackground and added to about

--- src/components/About.js
+++ src/components/About.js
@@ -5,21 +5,27 @@ import styled from 'styled-components';
 const TextSection = styled.div`
   flex: 1; // Takes up the remaining space
   padding: 20px 80px; // Padding to space out the text from the image
-  color: #7e9278; // Soft green text color
+  color: #bdd2c5;
 `;
 
+const StyledAbout = styled.main`
+  background-color: #101810; // Light green
+`;
 
 function About() {
   return (
+    <StyledAbout>
     <TextSection>
 
     <h2> Electrical & Computer Engineering Student</h2>
     <p>
-        Hi! My name is Yulu <i>/you-loo</i>; I was born in Shenzhen, China but have been living in Canada for more than half my life now.
-        
+        Hi! My name is Yulu <i>/you-loo</i>; I've been playing tennis since I was 8, I'm an enormous bookworm, 
+        and I possess a curiosity for everything that drives me to continuously learn and explore new things.
     </p>
+    <p>THe majority of my experiences are in the field of autonomous vehicles</p>
  
     </TextSection>
+    </StyledAbout>
   );
 }
 
