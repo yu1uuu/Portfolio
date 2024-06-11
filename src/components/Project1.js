10d7617393818da34d1e656d81bd72817c0ac695
yu1uuu <yulujiang00@gmail.com>
2024-06-11 14:33:51 -0400

    refined more

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -18,22 +18,22 @@ function Project1() {
     <TextSection>
     <div className="project-details">
       <h1>Cone Map</h1>
-      <p>As the mapping lead of the driverless section of University of Toronto's formula racing team, we work to produce a cone map</p>
-      <section>
-        <h2>Project Description</h2>
-        <p>Provide a detailed description of what the project entails, its scope, and its objectives.</p>
-      </section>
-      <section>
-        <h2>Project Features</h2>
-        <ul>
-          <li>Feature 1: Description of feature 1.</li>
-          <li>Feature 2: Description of feature 2.</li>
-          <li>Feature 3: Description of feature 3.</li>
-        </ul>
-      </section>
+      <p>As the mapping lead of the driverless section of University of Toronto's formula racing team, my team and I work to produce a cone map</p>
+
       <section>
         <h2>Technical Information</h2>
-        <p>Discuss the technologies used in the project, libraries, frameworks, and any technical challenges overcome.</p>
+        <p>Mapping is mainly consisted of 3 nodes. EKF node, build graph node, and compute graph node</p>
+        <ul>
+          <li>EKF: the ekf performs localization of the car, taking in both error values of GPS and IMU. The sensors are fused using the
+            extended kalman filter, as the name suggests </li>
+          <li>Build Graph: build graph node does three main things. It firts uses a KD tree and data association to locate and label the 
+            nearest cone. Then it performs loop closure to gain an absolute ground truth point and correct all the past errors. Lastly, 
+            it stores all edges and vertices for graphSLAM
+          </li>
+          <li>Compute Graph: this node convert everything in build graph into G2O types and adds them to the G2O optimizer. Finally,
+          it publishes the cone map and the updated pose
+          </li>
+        </ul>
       </section>
       <section>
         <h2>Conclusion and Future Work</h2>
