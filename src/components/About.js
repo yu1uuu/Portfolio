c64d5d9f6cacb3e7e35cf78c6558f7faeb92db8a
yu1uuu <yulujiang00@gmail.com>
2024-06-20 10:22:08 -0400

    finalized blurb

--- src/components/About.js
+++ src/components/About.js
@@ -19,10 +19,16 @@ function About() {
 
     <h2> Electrical & Computer Engineering Student</h2>
     <p>
-        Hi! My name is Yulu <i>/you-loo</i>; I'm an enormous bookworm who possess a curiosity for everything,
-        driving me to continuously learn and explore new things.
-    </p>
-    <p>Intrigued about the concepts behind autonomous driving, I joined the diverless section of our schools formula racing team.</p>
+    Hi! My name is Yulu <i>/you-loo</i>; I'm an enormous bookworm with a curiosity for everything, driving me to continuously learn and explore new things.
+</p>
+<p>
+    Intrigued by the concepts behind autonomous driving, I joined the driverless section of the University of Toronto's formula racing team. I quickly immersed myself in learning all I could about the mapping section and became the lead the next year. To further expand my knowledge, I pursued a research opportunity concerning lane detection using machine learning.
+</p>
+<p>
+    In addition to my work in autonomous vehicles, I am starting a new project in cybersecurity, focusing on detecting the use of AI like ChatGPT in published papers. I have also developed an interest in front-end development, as it is a field that demonstrates immediate results. 
+    My goal is to push the boundaries in areas like autonomous vehicles, cybersecurity, and front-end development, leveraging my diverse interests and skills to create impactful solutions.
+</p>
+
  
     </TextSection>
     </StyledAbout>
