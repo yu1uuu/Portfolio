4fb550252cf249531fa4b64951c602c72f9213fd
yu1uuu <yulujiang00@gmail.com>
2024-08-25 17:41:00 -0400

    added socials on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'; // Importing icons
 
 // Define the styled component with proper capitalization
 const StyledFooter = styled.footer`
@@ -7,12 +8,41 @@ const StyledFooter = styled.footer`
   color: white;
   padding: 10px;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const IconContainer = styled.div`
+  margin: 10px 0; /* Space between the icons and the copyright text */
+`;
+
+const IconLink = styled.a`
+  color: white;
+  margin: 0 10px;
+  font-size: 24px;
+  text-decoration: none;
+
+  &:hover {
+    color: #ddd; /* Slight color change on hover */
+  }
 `;
 
 // Use the StyledFooter component in your Footer component
 function Footer() {
   return (
     <StyledFooter>
+      <IconContainer>
+        <IconLink href="https://www.linkedin.com/in/yul-jiang/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </IconLink>
+        <IconLink href="https://github.com/yu1uuu" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </IconLink>
+        <IconLink href="mailto:yulujiang00@gmail.com">
+          <FaEnvelope />
+        </IconLink>
+      </IconContainer>
       <p>Copyright © 2024 Yulu Jiang. All rights reserved.</p>
     </StyledFooter>
   );
